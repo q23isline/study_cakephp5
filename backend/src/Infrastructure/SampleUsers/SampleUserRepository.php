ea7e845b5979a14939a1007b9bbe161dd7fb1fc9
q23isline <rowpure@gmail.com>
2025-02-05 23:29:45 +0900

    add: サンプルユーザーのCRUD APIがDBのデータを元にレスポンスする

--- backend/src/Infrastructure/SampleUsers/SampleUserRepository.php
+++ backend/src/Infrastructure/SampleUsers/SampleUserRepository.php
@@ -3,8 +3,9 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\SampleUsers;
 
+use Cake\I18n\Date;
+use Cake\ORM\TableRegistry;
 use DateTime;
-use Exception;
 
 final class SampleUserRepository
 {
@@ -16,11 +17,14 @@ final class SampleUserRepository
      */
     public function countUsers(array $command): int
     {
-        // 実際はコマンド変数から SQL クエリを組み立てる
-        $query = $command;
-        // 実際はモデルを利用してデータを取得する
-        $count = $query;
-        $count = 4;
+        $conditions = null;
+        if ($command['filterName'] !== null) {
+            $conditions['name LIKE'] = '%' . $command['filterName'] . '%';
+        }
+
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $count = $model->find()->where($conditions)->count();
 
         return $count;
     }
@@ -33,19 +37,37 @@ final class SampleUserRepository
      */
     public function findUsers(array $command): array
     {
-        // 実際はコマンド変数から SQL クエリを組み立てる
-        $query = $command;
-        // 実際はモデルを利用してデータを取得する
+        $conditions = null;
+        if ($command['filterName'] !== null) {
+            $conditions['name LIKE'] = '%' . $command['filterName'] . '%';
+        }
+
+        $offset = ($command['pageNumber'] - 1) * $command['pageSize'];
+
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $query = $model->find()->where($conditions)->offset($offset)->limit($command['pageSize']);
+
+        if ($command['sort'] !== null) {
+            $sortColumn = $this->removeLeadingHyphen($command['sort']);
+            $order = $this->startsWithHyphen($command['sort']) ? 'DESC' : 'ASC';
+            $query = $query->orderBy([
+                $sortColumn => $order,
+                'id' => 'ASC', // 並び順が一定になるように ID をつけとく
+            ]);
+        }
+
+        /** @var array<\App\Model\Entity\SampleUser> $records */
         $records = $query;
 
         $result = [];
         foreach ($records as $record) {
             $result[] = [
-                'id' => (int)($record['id'] ?? 1),
-                'name' => $record['name'] ?? '田中 太郎',
-                'birthDay' => new DateTime($record['birth_day'] ?? '2000-01-01'),
-                'height' => $record['height'] ?? '170.0',
-                'gender' => $record['gender'] ?? '1',
+                'id' => $record->id,
+                'name' => $record->name,
+                'birthDay' => new DateTime($record->birth_day->format('Y-m-d')),
+                'height' => $record->height,
+                'gender' => $record->gender,
             ];
         }
 
@@ -60,9 +82,16 @@ final class SampleUserRepository
      */
     public function isExistUser(int $id): bool
     {
-        // 実際はモデルを利用してデータを取得する
-        $record = $id;
-        $record = $record;
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $query = $model->find()->select(['id'])->where(['id' => $id])->first();
+
+        /** @var \App\Model\Entity\SampleUser|null $record */
+        $record = $query;
+
+        if ($record === null) {
+            return false;
+        }
 
         return true;
     }
@@ -75,15 +104,16 @@ final class SampleUserRepository
      */
     public function findUser(int $id): array
     {
-        // 実際はモデルを利用してデータを取得する
-        $record = $id;
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $record = $model->get($id);
 
         $result = [
-            'id' => (int)($record['id'] ?? 1),
-            'name' => (string)($record['name'] ?? '田中 太郎'),
-            'birthDay' => new DateTime($record['birth_day'] ?? '2000-01-01'),
-            'height' => (string)($record['height'] ?? '170.0'),
-            'gender' => (string)($record['gender'] ?? '1'),
+            'id' => $record->id,
+            'name' => $record->name,
+            'birthDay' => new DateTime($record->birth_day->format('Y-m-d')),
+            'height' => $record->height,
+            'gender' => $record->gender,
         ];
 
         return $result;
@@ -97,20 +127,17 @@ final class SampleUserRepository
      */
     public function saveUser(array $command): int
     {
-        $saveData = [
-            'Users' => [
-                'name' => $command['name'],
-                'birth_day' => $command['birthDay']->format('Y-m-d'),
-                'height' => $command['height'],
-                'gender' => $command['gender'],
-            ],
-        ];
-
-        // 実際はモデルを利用してデータを保存する
-        $saveData = $saveData;
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $entity = $model->newEmptyEntity();
+        $entity->name = $command['name'];
+        $entity->birth_day = new Date($command['birthDay']->format('Y-m-d'));
+        $entity->height = $command['height'];
+        $entity->gender = $command['gender'];
+        $saved = $model->saveOrFail($entity);
 
         // 保存後に採番された ID を返す
-        return 1;
+        return $saved->id;
     }
 
     /**
@@ -121,17 +148,14 @@ final class SampleUserRepository
      */
     public function updateUser(array $command): void
     {
-        $saveData = [
-            'Users' => [
-                'name' => $command['name'],
-                'birth_day' => $command['birthDay']->format('Y-m-d'),
-                'height' => $command['height'],
-                'gender' => $command['gender'],
-            ],
-        ];
-
-        // 実際はモデルを利用してデータを保存する
-        $saveData = $saveData;
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $entity = $model->get($command['id']);
+        $entity->name = $command['name'];
+        $entity->birth_day = new Date($command['birthDay']->format('Y-m-d'));
+        $entity->height = $command['height'];
+        $entity->gender = $command['gender'];
+        $model->saveOrFail($entity);
     }
 
     /**
@@ -142,11 +166,31 @@ final class SampleUserRepository
      */
     public function deleteUser(int $id): void
     {
-        // 実際はモデルを利用してデータを削除する
-        $result = $id;
+        /** @var \App\Model\Table\SampleUsersTable $model */
+        $model = TableRegistry::getTableLocator()->get('SampleUsers');
+        $entity = $model->get($id);
+        $model->deleteOrFail($entity);
+    }
 
-        if (!$result) {
-            throw new Exception('削除に失敗しました。');
-        }
+    /**
+     * 先頭がハイフンかどうか
+     *
+     * @param string $str
+     * @return bool
+     */
+    private function startsWithHyphen(string $str): bool
+    {
+        return isset($str[0]) && $str[0] === '-';
+    }
+
+    /**
+     * 先頭のハイフンを除いた文字列を取得する
+     *
+     * @param string $str
+     * @return string
+     */
+    private function removeLeadingHyphen(string $str): string
+    {
+        return $this->startsWithHyphen($str) ? substr($str, 1) : $str;
     }
 }
