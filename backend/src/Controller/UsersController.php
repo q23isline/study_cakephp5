7c19de9b84a8979aa7ee1059f2625247451dbf25
rowpure <rowpure@gmail.com>
2025-02-08 11:19:47 +0900

    add: ログインコントローラー生成 `docker compose exec backend bin/cake bake controller users --actions login`

---
+++ backend/src/Controller/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ */
+class UsersController extends AppController
+{
+    /**
+     * Login method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function login()
+    {
+        $this->request->allowMethod(['get', 'post']);
+        $result = $this->Authentication->getResult();
+        if ($result->isValid()) {
+            $this->Flash->success(__('Login successful'));
+            $redirect = $this->Authentication->getLoginRedirect();
+            if ($redirect) {
+                return $this->redirect($redirect);
+            }
+        }
+
+        // Display error if user submitted and authentication failed
+        if ($this->request->is('post')) {
+            $this->Flash->error(__('Invalid username or password'));
+        }
+    }
+}
---
+++ backend/tests/TestCase/Controller/UsersControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\UsersController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\UsersController Test Case
+ *
+ * @uses \App\Controller\UsersController
+ */
+class UsersControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var list<string>
+     */
+    protected array $fixtures = [
+        'app.Users',
+    ];
+
+    /**
+     * Test login method
+     *
+     * @return void
+     * @uses \App\Controller\UsersController::login()
+     */
+    public function testLogin(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
