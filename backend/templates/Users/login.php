22b3c2d452b94a2caf90e407163fc10c76a4b6ef
rowpure <rowpure@gmail.com>
2025-02-08 13:54:10 +0900

    add: ログインテンプレートをチュートリアルに合わせて変更

--- backend/templates/Users/login.php
+++ backend/templates/Users/login.php
@@ -4,12 +4,14 @@
  */
 ?>
 <div class="users form content">
+    <?= $this->Flash->render() ?>
+    <h3>Login</h3>
     <?= $this->Form->create() ?>
     <fieldset>
         <legend><?= __('Please enter your username and password') ?></legend>
-        <?= $this->Form->control('username') ?>
-        <?= $this->Form->control('password') ?>
+        <?= $this->Form->control('email', ['required' => true]) ?>
+        <?= $this->Form->control('password', ['required' => true]) ?>
     </fieldset>
-    <?= $this->Form->button(__('Login')); ?>
+    <?= $this->Form->submit(__('Login')); ?>
     <?= $this->Form->end() ?>
 </div>
