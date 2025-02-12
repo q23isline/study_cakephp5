7cadc055aee390f32fb5328e6b464ad6bccd2d48
q23isline <rowpure@gmail.com>
2025-02-12 19:48:18 +0900

    add: セッションテーブル定義がCakePHPの指定になるようマイグレーションファイル変更

--- backend/config/Migrations/20250208061155_CreateSessions.php
+++ backend/config/Migrations/20250208061155_CreateSessions.php
@@ -12,36 +12,44 @@ class CreateSessions extends BaseMigration
      *
      * More information on this method is available here:
      * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
      * @return void
      */
     public function change(): void
     {
         $table = $this->table('sessions');
+
         $table->addColumn('id', 'char', [
-            'default' => null,
             'limit' => 40,
             'null' => false,
         ]);
+
         $table->addColumn('created', 'datetime', [
-            'default' => null,
+            'default' => 'CURRENT_TIMESTAMP',
             'null' => false,
         ]);
+
         $table->addColumn('modified', 'datetime', [
-            'default' => null,
+            'default' => 'CURRENT_TIMESTAMP',
+            'update' => 'CURRENT_TIMESTAMP',
             'null' => false,
         ]);
+
         $table->addColumn('data', 'binary', [
             'default' => null,
+            'limit' => 8000,
             'null' => true,
         ]);
+
         $table->addColumn('expires', 'integer', [
             'default' => null,
             'limit' => 11,
             'null' => true,
+            'signed' => false,
         ]);
-        $table->addPrimaryKey([
-            'id',
-        ]);
+
+        $table->addPrimaryKey(['id']);
+
         $table->create();
     }
 }
