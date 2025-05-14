cace48f90a1487f5224f6df06b23769611fad504
q23isline <rowpure@gmail.com>
2025-05-14 23:55:00 +0900

    add: CSRFトークン取得APIは認証不要にする

--- backend/src/Controller/Api/V1/CsrfTokenGetController.php
+++ backend/src/Controller/Api/V1/CsrfTokenGetController.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 namespace App\Controller\Api\V1;
 
 use App\Controller\Api\AppApiController;
+use Cake\Event\EventInterface;
 
 class CsrfTokenGetController extends AppApiController
 {
+    /**
+     * @param \Cake\Event\EventInterface<\Cake\Controller\Controller> $event
+     * @return void
+     * @link https://book.cakephp.org/5/en/tutorials-and-examples/cms/authentication.html
+     */
+    public function beforeFilter(EventInterface $event): void
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->addUnauthenticatedActions(['invoke']);
+    }
+
     /**
      * @return void
      */
